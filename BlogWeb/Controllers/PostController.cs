810d899c33c9cf9b4bfc1dc073a52a6249e35dd0
Caelum <caelum_rio@hotmail.com>
2018-04-28 15:14:13 -0300

    Segundo dia de curso

---
+++ BlogWeb/Controllers/PostController.cs
@@ -0,0 +1,61 @@
+﻿using BlogWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogWeb.Controllers
+{
+    public class PostController : Controller
+    {
+
+        // GET: Post
+        public ActionResult Index()
+        {
+            var posts = new List<Post>();
+            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(stringConexao))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "select * from Posts";
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    Post post = new Post()
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Titulo = Convert.ToString(reader["titulo"]),
+                        Resumo = Convert.ToString(reader["resumo"]),
+                        Categoria = Convert.ToString(reader["categoria"])
+                    };
+                    posts.Add(post);
+                }
+            }
+            return View(posts);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(Post post)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(stringConexao))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "insert into Posts(titulo, resumo, categoria) values ('"+post.Titulo+"','"+post.Resumo+"','"+post.Categoria+"')";
+                command.ExecuteNonQuery();
+            }
+        
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ BlogWeb/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogWeb.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Resumo { get; set; }
+        public string Categoria { get; set; }
+
+    }
+}
